799e5698e8b8e5dc40b6ad1d0aa946e64d807dd3
Keijiro Takahashi <keijiro@gmail.com>
2025-12-16 16:49:06 +0900

    Multiscreen

---
+++ Assets/06 Monitor/Scripts/ProjectorController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using VJUITK;
+
+namespace Fluo {
+
+public sealed class ProjectorController : MonoBehaviour
+{
+    [SerializeField] UIDocument _monitor = null;
+
+    VisualElement _projector;
+    VJKnob _knob;
+
+    void Start()
+    {
+        var displays = Display.displays;
+        if (displays.Length > 1) displays[1].Activate();
+
+        _projector = GetComponent<UIDocument>().rootVisualElement.Q("root");
+        _knob = _monitor.rootVisualElement.Q<VJKnob>("knob-fadeout");
+    }
+
+    void Update()
+      => _projector.style.unityBackgroundImageTintColor
+           = new StyleColor(Color.white * (1 - _knob.value));
+}
+
+} // namespace Fluo
