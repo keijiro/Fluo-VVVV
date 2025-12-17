33d8043dfe2825b91c33a7afeaf01171484d0af0
Keijiro Takahashi <keijiro@gmail.com>
2025-12-17 11:33:48 +0900

    Use remote input for projector control

--- Assets/06 Monitor/Scripts/ProjectorController.cs
+++ Assets/06 Monitor/Scripts/ProjectorController.cs
@@ -1,30 +1,24 @@
 using UnityEngine;
 using UnityEngine.UIElements;
-using VJUITK;
 
 namespace Fluo {
 
 public sealed class ProjectorController : MonoBehaviour
 {
-    [SerializeField] UIDocument _monitor = null;
+    public void SetFadeout(float value)
+      => _projector.style.unityBackgroundImageTintColor
+           = new StyleColor(Grayscale(1 - value));
+
+    static Color Grayscale(float v) => new Color(v, v, v);
 
     VisualElement _projector;
-    VJKnob _knob;
 
     void Start()
     {
         var displays = Display.displays;
         if (displays.Length > 1) displays[1].Activate();
-
         _projector = GetComponent<UIDocument>().rootVisualElement.Q("root");
-        _knob = _monitor.rootVisualElement.Q<VJKnob>("knob-fadeout");
     }
-
-    void Update()
-      => _projector.style.unityBackgroundImageTintColor
-           = new StyleColor(Grayscale(1 - _knob.value));
-
-    static Color Grayscale(float v) => new Color(v, v, v);
 }
 
 } // namespace Fluo
